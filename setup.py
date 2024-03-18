ea5dc3825ab68d22a6205911417b1e3c035e5eeb
Amrish Parmar <9840798+amrishparmar@users.noreply.github.com>
2024-03-18 12:47:49 +0000

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="secrets-management",
-    version="0.0.9",
+    version="0.1.0",
     author="Beauhurst",
     author_email="noreply@beauhurst.com",
     description="Helper library for interacting with AWS Secrets Manager",
