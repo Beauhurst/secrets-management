c0647cf25a79a9249fb1c0767a92d47296c98e76
Amrish Parmar <amrish.parmar@beauhurst.com>
2026-06-10 17:36:02 +0100

    Drop support for Python < 3.10

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.10",
     install_requires=[
         "boto3",
         "django-environ",
